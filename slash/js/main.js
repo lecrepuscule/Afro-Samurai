9e202fab84ef22be569e0aa199ef9b69a540d1e2
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-22 10:10:58 +0100

    added a single slashing line and key press listener

--- slash/js/main.js
+++ slash/js/main.js
@@ -3,24 +3,38 @@ $(document).ready(function(){
 })
 
 function initGame(){
-  $(".game-space").on("click", function(e){
-    console.log(e);
-  })
+  $("#red-dot-1").offset({top:206,left:450});
+  $("#red-dot-2").offset({top:506,left:850});
+  var slashingLine1 = {
+      upperEnd: [206, 450],
+      lowerEnd: [506, 850],
+      gradient: (506-206)/(850-450)
+    };
+  $("body").on("keypress", keypressHandler);
+}
+
+function keypressHandler(e){
+  console.log(e);
+}
+
+function playGame(){
+  pickHeight();
+  pickSpeed();
+  moveObject();
+}
+
+function pickHeight(){
+
+}
+
   /*define height and width of the tunnel*/
 
-  /*define slashing line
-    slashingLine = {
-      upperEnd: []
-      lowerEnd: []
-      gradient: 
-    }
-  */
 
   // $('.background').on("keypress", function(e){
   //   console.log(e.offsetX);
   //   console.log(e.offsetY);
   // })
-}
+
 
 
   /*define height and width of the tunnel*/
